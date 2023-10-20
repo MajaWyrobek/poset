8a6304a626c73cfa3021f603d6f498f246082499
Kamil Dybek <kam.dyb1@gmail.com>
2023-10-20 19:45:48 +0200

    Changed poset to a better data structure.

--- poset.cc
+++ poset.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <set>
 #include "poset.h"
 
 #define CHECK_FOR_NULLPTR(ptr) \
@@ -9,14 +10,15 @@
 
 using std::unordered_map;
 using std::string;
-using std::vector;
+using std::pair;
+using std::set;
 
-static unordered_map<unsigned long, unordered_map<string, vector<string>>> posets;
+static unordered_map<unsigned long, pair<set<string>, set<pair<string, string>>>> posets;
 static unsigned long next_id = 0;
 
 extern "C" {
     unsigned long poset_new() {
-        unordered_map<string, vector<string>> poset;
+        pair<set<string>, set<pair<string, string>>> poset;
         unsigned long poset_id = next_id;
         next_id++;
         posets[poset_id] = poset;
@@ -30,7 +32,7 @@ extern "C" {
     size_t poset_size(unsigned long id) {
         size_t size;
         try {
-            size = posets.at(id).size();
+            size = std::get<0>(posets.at(id)).size();
         }
         catch (std::exception &e) {
             size = 0;
@@ -38,22 +40,6 @@ extern "C" {
         return size;
     }
 
-    bool poset_insert(unsigned long id, char const* value) {
-        CHECK_FOR_NULLPTR(value)
-        try {
-            unordered_map<string, vector<string>> poset = posets.at(id);
-            string new_element(value);//makes a deep copy
-            if(poset.count(new_element) != 0) {
-                return false;
-            }
-            vector<string> relations;
-            poset[new_element] = relations;
-            return true;
-        }
-        catch (std::exception &e) {
-            return false;
-        }
-    }
 }
 
 int main() {
