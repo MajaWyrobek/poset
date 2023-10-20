f476a21f2eaf138c3ebeedbbdb0d80cc7ead4e93
Kamil Dybek <kam.dyb1@gmail.com>
2023-10-20 20:31:42 +0200

    Feature: poset_insert

--- poset.cc
+++ poset.cc
@@ -12,13 +12,19 @@ using std::unordered_map;
 using std::string;
 using std::pair;
 using std::set;
+using std::vector;
 
-static unordered_map<unsigned long, pair<set<string>, set<pair<string, string>>>> posets;
+using elements_container = set<string>;
+using relation = pair<string, string>;
+using relations_container = set<relation>;
+using poset = pair<elements_container, relations_container>;
+
+static unordered_map<unsigned long, poset> posets;
 static unsigned long next_id = 0;
 
 extern "C" {
     unsigned long poset_new() {
-        pair<set<string>, set<pair<string, string>>> poset;
+        poset poset;
         unsigned long poset_id = next_id;
         next_id++;
         posets[poset_id] = poset;
@@ -38,6 +44,27 @@ extern "C" {
             size = 0;
         }
         return size;
+    }
+
+    bool poset_insert(unsigned long id, char const* value) {
+        CHECK_FOR_NULLPTR(value)
+        try {
+            poset poset = posets.at(id);
+            elements_container poset_elements = std::get<0>(poset);
+            relations_container poset_relations = std::get<1>(poset);
+            string new_element(value);//makes a deep copy
+            relation new_relation = std::make_pair(value, value);
+
+            if(poset_elements.count(new_element) != 0) {
+                return false;
+            }
+            poset_elements.insert(new_element);
+            poset_relations.insert(new_relation);
+            return true;
+        }
+        catch (std::exception &e) {
+            return false;
+        }
     }
 
 }
