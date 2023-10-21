82c6754e83d1cff8e627cec8b910a22fef405061
Kamil Dybek <kam.dyb1@gmail.com>
2023-10-21 22:59:36 +0200

    Feature: element indexing (the const string& idea didn't work)

--- poset.cc
+++ poset.cc
@@ -17,14 +17,16 @@ using std::make_pair;
                 return false;
 
 /* Container Aliases */
-using elements_container = set<string>;
-using relation = pair<const string&, const string&>;
+using elements_container = unordered_map<string, unsigned long>;
+//unsigned longs are quicker to compare than strings longer than 4
+using relation = pair<unsigned long, unsigned long>;
 using relations_container = set<relation>;
 using poset = pair<elements_container, relations_container>;
 
 /* Global Variables */
 static unordered_map<unsigned long, poset> posets;
-static unsigned long next_id = 0;
+static unsigned long next_poset_id = 0;
+static unsigned long next_element_id = 0;
 
 /* Auxiliary Functions */
 static void poset_remove_relations(relations_container& relations,
@@ -38,7 +40,7 @@ static void poset_remove_relations(relations_container& relations,
 
 static vector<relation> poset_relations_with_element(
         const relations_container& relations,
-        const string& element)
+        unsigned long element)
 {
     vector<relation> relations_with_element;
     for(const auto& relation : relations)
@@ -59,12 +61,12 @@ static bool poset_transitive_closure(relations_container& relations,
     do
     {
         missing_relations.clear();
-        for(auto i = relations.begin(); i != relations.end(); i++)
+        for(auto i = relations.cbegin(); i != relations.cend(); i++)
         {
-            for (auto j = relations.begin(); j != relations.end(); j++)
+            for (auto j = relations.cbegin(); j != relations.cend(); j++)
             {
-                if (j->first == i->second
-                    && relations.count(make_pair(i->first, j->second)) == 0)
+                if (j->first == i->second &&
+                    relations.count(make_pair(i->first, j->second)) == 0)
                 {
                     if(relations.count(make_pair(j->second, i->first)) != 0)
                     {
@@ -89,8 +91,8 @@ extern "C"
     unsigned long poset_new()
     {
         poset poset;
-        unsigned long poset_id = next_id;
-        next_id++;
+        unsigned long poset_id = next_poset_id;
+        next_poset_id++;
         posets[poset_id] = poset;
         return poset_id;
     }
@@ -121,17 +123,16 @@ extern "C"
         CHECK_FOR_NULLPTR(value)
         try
         {
-            poset poset = posets.at(id);
-            elements_container elements = poset.first;
-            relations_container relations = poset.second;
+            poset& poset = posets.at(id);
+            elements_container& elements = poset.first;
             string new_element(value);//Makes a deep copy.
 
             if(elements.count(new_element) != 0)
             {
                 return false;
             }
-            //relation of an element with itself is held implicitely.
-            elements.insert(new_element);
+            //Relation of an element with itself is held implicitely.
+            elements[new_element] = next_element_id++;
             return true;
         }
         catch (std::exception& e)
@@ -145,16 +146,16 @@ extern "C"
         CHECK_FOR_NULLPTR(value)
         try
         {
-            poset poset = posets.at(id);
-            elements_container elements = poset.first;
-            relations_container relations = poset.second;
-            string element_to_remove(value);
+            poset& poset = posets.at(id);
+            elements_container& elements = poset.first;
+            relations_container& relations = poset.second;
+            auto element_to_remove = elements[value];
             vector<relation> relations_to_remove =
-                    poset_relations_with_element(relations,element_to_remove);
+                    poset_relations_with_element(relations, element_to_remove);
 
             poset_remove_relations(relations, relations_to_remove);
             //Erase returns 0 if nothing was removed.
-            if(elements.erase(element_to_remove) == 0)
+            if(elements.erase(value) == 0)
             {
                 return false;
             }
@@ -172,21 +173,22 @@ extern "C"
         CHECK_FOR_NULLPTR(value2)
         try
         {
-            poset poset = posets.at(id);
-            elements_container elements = poset.first;
-            relations_container relations = poset.second;
-            string element1(value1);
-            string element2(value2);
+            poset& poset = posets.at(id);
+            elements_container& elements = poset.first;
+            relations_container& relations = poset.second;
+            auto element1 = elements[value1];
+            auto element2 = elements[value2];
             vector<relation> added_relations;
 
-            if(elements.count(element1) == 0 ||
-               elements.count(element2) == 0 ||
+            if(elements.count(value1) == 0 ||
+               elements.count(value2) == 0 ||
                relations.count(make_pair(element1, element2)) != 0 ||
                relations.count(make_pair(element2, element1)) != 0)
             {
                 return false;
             }
             relations.insert(make_pair(element1, element2));
+            added_relations.emplace_back(element1, element2);
             if(!poset_transitive_closure(relations, added_relations))
             {
                 poset_remove_relations(relations, added_relations);
