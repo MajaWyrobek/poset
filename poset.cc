3ea3f7a9a5620986fb0c8c2779d9fa56ed6a0197
Nadia Hoffmann <hoffmann.nadia@outlook.com>
2023-10-21 17:01:01 +0200

    Moved new features to a branch

--- poset.cc
+++ poset.cc
@@ -40,40 +40,6 @@ extern "C" {
         return size;
     }
 
-    bool poset_del(unsigned long id, char const *value1, char const *value2) {
-        // sprawdzanie, czy nie zaburzy to przechodniości
-        // czy w add nie może być tak, że zaburzy się antysymetryczność?
-    }
-
-    bool poset_test(unsigned long id, char const* value1, char const* value2) {
-        // tu brakuje case, że element jest sam ze sobą w relacji;
-        // implementujemy to w funkcji, czy w secie?
-        try {
-            pair<string, string> order(value1, value2); 
-            if (std::get<1>(posets.at(id)).find(order) != std::get<1>(posets.at(id)).end()) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        catch (std::exception &e) {
-            return false;
-        }
-    }
-
-    void poset_clear(unsigned long id) {
-        try {
-            posets.erase(id);
-            pair<set<string>, set<pair<string, string>>> poset;
-            unsigned long poset_id = id;
-            posets[poset_id] = poset;
-        }
-        catch (std::exception &e) {
-            std::cerr << "poset_clear: poset " << id << " does not exist\n";
-        }
-    }
-
 }
 
 int main() {
