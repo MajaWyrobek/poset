5f58ca7e32784e10fb868bbcc21d9b6e44811e83
Kamil Dybek <kam.dyb1@gmail.com>
2023-10-22 18:21:47 +0200

    Added cerr messages

--- poset.cc
+++ poset.cc
@@ -3,6 +3,8 @@
 #include <vector>
 #include <unordered_set>
 #include <algorithm>
+#include <string>
+#include <cassert>
 #include "poset.h"
 
 using std::unordered_map;
@@ -12,10 +14,7 @@ using std::unordered_set;
 using std::vector;
 using std::make_pair;
 
-#define CHECK_FOR_NULLPTR(ptr) \
-            if(ptr == NULL)    \
-                return false;
-
+//There is no hashing function for pairs in the standard library
 struct pair_hush
 {
     size_t operator()(const pair<unsigned long, unsigned long> &pair) const
@@ -35,9 +34,12 @@ using relations_container = unordered_set<relation, pair_hush>;
 using poset = pair<elements_container, relations_container>;
 
 /* Global Variables */
-static unordered_map<unsigned long, poset> posets;
-static unsigned long next_poset_id = 0;
-static unsigned long next_element_id = 0;
+namespace
+{
+    unordered_map<unsigned long, poset> posets;
+    unsigned long next_poset_id = 0;
+    unsigned long next_element_id = 0;
+}
 
 /* Auxiliary Functions */
 static void poset_remove_relations(relations_container& relations,
@@ -96,124 +98,353 @@ static bool poset_transitive_closure(relations_container& relations,
     return true;
 }
 
-/* Main Functions */
-extern "C"
+namespace message
 {
-    unsigned long poset_new()
+    static string print_value(const char* value)
     {
-        poset poset;
-        unsigned long poset_id = next_poset_id;
-        next_poset_id++;
-        posets[poset_id] = poset;
-        return poset_id;
-    }
-
-    void poset_delete(unsigned long id)
-    {
-        posets.erase(id);
-    }
-
-    size_t poset_size(unsigned long id)
-    {
-        size_t size;
-        try
+        if(value == nullptr)
         {
-            //If a poset with a given id doesn't exist in posets,
-            //at will throw an out_of_range exception.
-            size = posets.at(id).first.size();
+            return "NULL";
         }
-        catch (std::exception& e)
+        else
         {
-            size = 0;
-        }
-        return size;
-    }
-
-    bool poset_insert(unsigned long id, char const* value)
-    {
-        CHECK_FOR_NULLPTR(value)
-        try
-        {
-            poset& poset = posets.at(id);
-            elements_container& elements = poset.first;
-            string new_element(value);//Makes a deep copy.
-
-            if(elements.count(new_element) != 0)
-            {
-                return false;
-            }
-            //Relation of an element with itself is held implicitely.
-            elements[new_element] = next_element_id++;
-            return true;
-        }
-        catch (std::exception& e)
-        {
-            return false;
+            return "\"" + string(value) + "\"";
         }
     }
 
-    bool poset_remove(unsigned long id, char const* value)
+    static void function_start(const string& function_name)
     {
-        CHECK_FOR_NULLPTR(value)
-        try
-        {
-            poset& poset = posets.at(id);
-            elements_container& elements = poset.first;
-            relations_container& relations = poset.second;
-            auto element_to_remove = elements[value];
-            vector<relation> relations_to_remove =
-                    poset_relations_with_element(relations, element_to_remove);
-
-            poset_remove_relations(relations, relations_to_remove);
-            //Erase returns 0 if nothing was removed.
-            if(elements.erase(value) == 0)
-            {
-                return false;
-            }
-            return true;
-        }
-        catch (std::exception& e)
-        {
-            return false;
-        }
+        std::cerr << function_name + "()" << std::endl;
     }
 
-    bool poset_add(unsigned long id, char const* value1, char const* value2)
+    static void function_start(const string& function_name, unsigned long id)
     {
-        CHECK_FOR_NULLPTR(value1)
-        CHECK_FOR_NULLPTR(value2)
-        try
-        {
-            poset& poset = posets.at(id);
-            elements_container& elements = poset.first;
-            relations_container& relations = poset.second;
-            auto element1 = elements[value1];
-            auto element2 = elements[value2];
-            vector<relation> added_relations;
+        std::cerr << function_name + "(" +
+                     std::to_string(id) +
+                     ")" << std::endl;
+    }
 
-            if(elements.count(value1) == 0 ||
-               elements.count(value2) == 0 ||
-               relations.count(make_pair(element1, element2)) != 0 ||
-               relations.count(make_pair(element2, element1)) != 0)
-            {
-                return false;
-            }
-            relations.insert(make_pair(element1, element2));
-            added_relations.emplace_back(element1, element2);
-            if(!poset_transitive_closure(relations, added_relations))
-            {
-                poset_remove_relations(relations, added_relations);
-                return false;
-            }
-            return true;
-        }
-        catch (std::exception& e)
-        {
-            return false;
-        }
+    static void function_start(const string& function_name, unsigned long id,
+                               const char* value)
+    {
+        std::cerr << function_name + "(" +
+                     std::to_string(id) +
+                     ", " +
+                     print_value(value) +
+                     ")" << std::endl;
+    }
+
+    static void function_start(const string& function_name, unsigned long id,
+                               const char* value1, const char* value2)
+    {
+        std::cerr << function_name + "(" +
+                     std::to_string(id) +
+                     ", " +
+                     print_value(value1) +
+                     ", " +
+                     print_value(value2) +
+                     ")" << std::endl;
+    }
+
+    static void about_poset(const string& function_name, unsigned long id,
+                            const string& message)
+    {
+        std::cerr << function_name + ": poset " +
+                     std::to_string(id) +
+                     " " +
+                     message << std::endl;
+    }
+
+    static void about_element(const string& function_name, unsigned long id,
+                              const char* value,
+                              const string& message)
+    {
+        std::cerr << function_name + ": poset " +
+                     std::to_string(id) +
+                     ", element " +
+                     print_value(value) +
+                     " " +
+                     message << std::endl;
+    }
+
+    static void about_relation(const string& function_name, unsigned long id,
+                              const char* value1, const char* value2,
+                              const string& message)
+    {
+        std::cerr << function_name + ": poset " +
+                     std::to_string(id) +
+                     ", relation (" +
+                     print_value(value1) +
+                     ", " +
+                     print_value(value2) +
+                     ") " +
+                     message << std::endl;
+    }
+
+    static void about_poset_size(const string& function_name, unsigned long id,
+                                 size_t size)
+    {
+        std::cerr << function_name + ": poset " +
+                     std::to_string(id) +
+                     " contains " +
+                     std::to_string(size) +
+                     " element(s)" << std::endl;
+    }
+
+    static void invalid_value(const string& function_name, const char* value,
+                              const string& value_descriptor)
+    {
+        std::cerr << function_name + ": invalid value" +
+                     value_descriptor + " (" +
+                     print_value(value) +
+                     ")" << std::endl;
     }
 }
 
+/* Main Functions */
+unsigned long poset_new()
+{
+    //-----------------------------
+    message::function_start(__func__);
+    //-----------------------------
+    poset poset;
+    unsigned long poset_id = next_poset_id;
+    next_poset_id++;
+    posets[poset_id] = poset;
+    //-----------------------------
+    message::about_poset(__func__, poset_id, "created");
+    //-----------------------------
+    return poset_id;
+}
+
+void poset_delete(unsigned long id)
+{
+    //-----------------------------
+    message::function_start(__func__, id);
+    //-----------------------------
+    if(posets.erase(id) == 0) {
+        //-----------------------------
+        message::about_poset(__func__, id, "does not exist");
+        //-----------------------------
+    }
+    else
+    {
+        //-----------------------------
+        message::about_poset(__func__, id, "deleted");
+        //-----------------------------
+    }
+}
+
+size_t poset_size(unsigned long id)
+{
+    //-----------------------------
+    message::function_start(__func__, id);
+    //-----------------------------
+    try
+    {
+        //If a poset with a given id doesn't exist in posets,
+        //at will throw an out_of_range exception.
+        size_t size = posets.at(id).first.size();
+        //-----------------------------
+        message::about_poset_size(__func__, id, size);
+        //-----------------------------
+        return size;
+    }
+    catch (std::out_of_range& e)
+    {
+        //-----------------------------
+        message::about_poset(__func__, id, "does not exist");
+        //-----------------------------
+        return 0;
+    }
+}
+
+bool poset_insert(unsigned long id, char const* value)
+{
+    //-----------------------------
+    message::function_start(__func__, id, value);
+    //-----------------------------
+    if(value == nullptr) {
+        //-----------------------------
+        message::invalid_value(__func__, value, "");
+        //-----------------------------
+        return false;
+    }
+    try
+    {
+        poset& poset = posets.at(id);
+        elements_container& elements = poset.first;
+        string new_element(value);//Makes a deep copy.
+
+        if(elements.count(new_element) != 0)
+        {
+            //-----------------------------
+            message::about_element(__func__, id, value, "already exists");
+            //-----------------------------
+            return false;
+
+        }
+        //Relation of an element with itself is held implicitely.
+        elements[new_element] = next_element_id++;
+        //-----------------------------
+        message::about_element(__func__, id, value, "inserted");
+        //-----------------------------
+        return true;
+    }
+    catch (std::out_of_range& e)
+    {
+        //-----------------------------
+        message::about_poset(__func__, id, "does not exist");
+        //-----------------------------
+        return false;
+    }
+}
+
+bool poset_remove(unsigned long id, char const* value)
+{
+    //-----------------------------
+    message::function_start(__func__, id, value);
+    //-----------------------------
+    if(value == nullptr) {
+        //-----------------------------
+        message::invalid_value(__func__, value, "");
+        //-----------------------------
+        return false;
+    }
+    if(posets.count(id) == 0)
+    {
+        //-----------------------------
+        message::about_poset(__func__, id, "does not exist");
+        //-----------------------------
+        return false;
+    }
+    poset& poset = posets.at(id);
+    elements_container& elements = poset.first;
+    relations_container& relations = poset.second;
+    if(elements.count(value) == 0)
+    {
+        //-----------------------------
+        message::about_element(__func__, id, value, "does not exist");
+        //-----------------------------
+        return false;
+    }
+    auto element_to_remove = elements.at(value);
+    elements.erase(value);
+    vector<relation> relations_to_remove =
+            poset_relations_with_element(relations, element_to_remove);
+    poset_remove_relations(relations, relations_to_remove);
+    //-----------------------------
+    message::about_element(__func__, id, value, "removed");
+    //-----------------------------
+    return true;
+}
+
+bool poset_add(unsigned long id, char const* value1, char const* value2)
+{
+    //-----------------------------
+    message::function_start(__func__, id, value1, value2);
+    //-----------------------------
+    bool invalid_data = false;
+    if(value1 == nullptr) {
+        //-----------------------------
+        message::invalid_value(__func__, value1, "1");
+        //-----------------------------
+        invalid_data = true;
+    }
+    if(value2 == nullptr) {
+        //-----------------------------
+        message::invalid_value(__func__, value2, "2");
+        //-----------------------------
+        invalid_data = true;
+    }
+    if(invalid_data) return false;
+
+    if(posets.count(id) == 0)
+    {
+        //-----------------------------
+        message::about_poset(__func__, id, "does not exist");
+        //-----------------------------
+        return false;
+    }
+    poset& poset = posets.at(id);
+    elements_container& elements = poset.first;
+    relations_container& relations = poset.second;
+    if(elements.count(value1) == 0)
+    {
+        //-----------------------------
+        message::about_element(__func__, id, value1, "does not exist");
+        //-----------------------------
+        return false;
+    }
+    auto element1 = elements.at(value1);
+    if(elements.count(value2) == 0)
+    {
+        //-----------------------------
+        message::about_element(__func__, id, value2, "does not exist");
+        //-----------------------------
+        return false;
+    }
+    auto element2 = elements.at(value2);
+    vector<relation> added_relations;
+
+    if(element1 == element2 ||
+       relations.count(make_pair(element1, element2)) != 0)
+    {
+        //-----------------------------
+        message::about_relation(__func__, id, value1, value2,
+                                "already exists");
+        //-----------------------------
+        return false;
+    }
+    if(relations.count(make_pair(element2, element1)) != 0)
+    {
+        //-----------------------------
+        message::about_relation(__func__, id, value1, value2,
+                                "cannot be added");
+        //-----------------------------
+        return false;
+    }
+    relations.insert(make_pair(element1, element2));
+    added_relations.emplace_back(element1, element2);
+    if(!poset_transitive_closure(relations, added_relations))
+    {
+        poset_remove_relations(relations, added_relations);
+        //-----------------------------
+        message::about_relation(__func__, id, value1, value2,
+                                "cannot be added");
+        //-----------------------------
+        return false;
+    }
+    //-----------------------------
+    message::about_relation(__func__, id, value1, value2, "added");
+    //-----------------------------
+    return true;
+}
+
 int main() {
+    unsigned long p1;
+    p1 = poset_new();
+    assert(poset_size(p1) == 0);
+    assert(poset_size(p1 + 1) == 0);
+    assert(!poset_insert(p1, NULL));
+    assert(poset_insert(p1, "A"));
+    assert(!poset_insert(p1, "A"));
+    assert(!poset_insert(p1 + 1, "B"));
+    assert(poset_size(p1) == 1);
+    assert(!poset_remove(p1 + 1, "A"));
+    assert(poset_remove(p1, "A"));
+    assert(!poset_remove(p1, "A"));
+    assert(poset_insert(p1, "B"));
+    assert(poset_insert(p1, "C"));
+    assert(poset_add(p1, "B", "C"));
+    assert(!poset_remove(p1, "A"));
+    assert(!poset_add(p1, NULL, "X"));
+    assert(!poset_add(p1, "X", NULL));
+    assert(!poset_add(p1, NULL, NULL));
+    assert(!poset_add(p1, "C", "D"));
+    assert(!poset_add(p1, "D", "C"));
+    assert(!poset_add(p1, "D", "D"));
+    assert(!poset_add(p1, "E", "D"));
+    assert(!poset_add(p1 + 1, "C", "D"));
     return 0;
 }
--- poset.h
+++ poset.h
@@ -1,26 +1,30 @@
-#ifndef CPP2_POSET_H
-#define CPP2_POSET_H
+#ifndef CPP_POSET_H
+#define CPP_POSET_H
 
-#endif //CPP2_POSET_H
+#ifdef __cplusplus
+    extern "C"
+    {
+#endif
+        unsigned long poset_new();
 
-extern "C" {
+        void poset_delete(unsigned long id);
 
-    unsigned long poset_new();
+        size_t poset_size(unsigned long id);
 
-    void poset_delete(unsigned long id);
+        bool poset_insert(unsigned long id, char const* value);
 
-    size_t poset_size(unsigned long id);
+        bool poset_remove(unsigned long id, char const* value);
 
-    bool poset_insert(unsigned long id, char const* value);
+        bool poset_add(unsigned long id, char const* value1, char const* value2);
 
-    bool poset_remove(unsigned long id, char const* value);
+        bool poset_del(unsigned long id, char const* value1, char const* value2);
 
-    bool poset_add(unsigned long id, char const* value1, char const* value2);
+        bool poset_test(unsigned long id, char const* value1, char const* value2);
 
-    bool poset_del(unsigned long id, char const* value1, char const* value2);
+        void poset_clear(unsigned long id);
 
-    bool poset_test(unsigned long id, char const* value1, char const* value2);
+#ifdef __cplusplus
+    }
+#endif
 
-    void poset_clear(unsigned long id);
-
-}
+#endif //CPP_POSET_H
